e95e8a4bd4c0a112c186c575318c3fd3a4c13b5a
NithinVikas-AV <nithinvikas8389@gmail.com>
2025-03-26 14:07:35 -0500

    feat: Added venv

--- HoneyPot.py
+++ HoneyPot.py
@@ -6,8 +6,8 @@ import os, sys
 log_name = "./log/"
 
 def main():
-    ip_add = "0.0.0.0"
-    port = 8080
+    ip_add = "192.168.188.97"
+    port = 85
     print("[+] ========== HoneyPot_Lite Activated ==========")
 
     try:
@@ -21,17 +21,15 @@ def main():
             client_con.send(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>You Have Been Hacked!</h1>")
             data = client_con.recv(2048)
             print(data.decode("utf-8"))
-            client_con.close() #newly added
+            client_con.close()
     except error as identifier:
         print("[+] Unspecified Error [{}]".format(identifier))
-    except KeyboardInterrupt as kbi:
+    except KeyboardInterrupt:
         print("[-] Process Stopped!")
     except OSError as ose:
         print("[+] OSError Error [{}]".format(ose))
-        get_socket_con.close()
     finally:
         get_socket_con.close()
-    get_socket_con.close()
 
 def scan_visitor(visitor_ip_address):
     today_date = date.today()
@@ -39,45 +37,44 @@ def scan_visitor(visitor_ip_address):
     dir_name = today_date.strftime("%d_%m_%Y")
     file_log_path = os.path.join(log_name, dir_name)
 
-    isExist = os.path.exists(file_log_path)
+    # Ensure log directory exists
+    if not os.path.exists(file_log_path):
+        os.makedirs(file_log_path, exist_ok=True)
 
-    if not isExist:
-        os.mkdir(file_log_path)
-    
-    file_log_name = "/"  visitor_ip_address.replace(".","_") + " " + datetime.strftime(datetime_now, "%d_%m_%Y") + ".log"
+    # Proper log file name
+    file_log_name = visitor_ip_address.replace(".", "_") + "_" + datetime.strftime(datetime_now, "%d_%m_%Y") + ".log"
+    log_file_path = os.path.join(file_log_path, file_log_name)
 
-    isFile_Exist = os.path.isfile(file_log_path + file_log_name)
-
-    if not isFile_Exist:
-        is_write_or_append = "w"
+    # Determine write mode
+    if os.path.isfile(log_file_path):
+        is_write_or_append = "a" 
     else:
-        is_write_or_append = "a"
-    
-    with open(file_log_path + file_log_name, is_write_or_append) as fp:
+        is_write_or_append = "w"
+
+    with open(log_file_path, is_write_or_append) as fp:
         get_port_details = get_port_information(visitor_ip_address)
-
         fp.write("\n")
-        for disp in range(len(get_port_details)-1):
-            fp.write(str(get_port_details[disp]) + "\n")
+        for disp in get_port_details:
+            fp.write(str(disp) + "\n")
         fp.write("\n")
-        fp.close()
 
-    print("[+] Visitor scanning ! {} complete ...".format(file_log_path + file_log_name))
+    print(f"[+] Visitor scanning ! {log_file_path} complete ...")
 
 def get_port_information(ip_address):
     scanner = nmap.PortScanner()
     scanner.scan(hosts=ip_address)
     ip_status = scanner[ip_address].state()
-    print("[+] Scanning Visitor Inprogress ...")
-    sc = {}
+    print("[+] Scanning Visitor In Progress ...")
+    details_info = []
+
     for host in scanner.all_hosts():
-        details_info = []
         for proto in scanner[host].all_protocols():
             lport = scanner[host][proto].keys()
             sc = scanner[host][proto]
             for port in lport:
-                a = "port: " + str(port) + "  Service Name: " + sc[port]['name'] + "  Product Name: " + sc[port]['product'] + "  Version Name: " + sc[port]['version'] + "  Status: " + sc[port]['status']
+                a = f"port: {port}  Service Name: {sc[port].get('name')}  Product Name: {sc[port].get('product')}  Version Name: {sc[port].get('version')}  Status: {sc[port].get('state')}"
                 details_info.append(a)
+
     return details_info
 
 if __name__ == "__main__":
